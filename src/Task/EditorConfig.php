972d8387d580375961461856581d22bf47d88da8
Thom Williams <thomscode@gmail.com>
2018-03-16 16:29:24 -0700

    Initial implementation

--- src/Task/EditorConfig.php
+++ src/Task/EditorConfig.php
@@ -32,15 +32,24 @@ class EditorConfig extends AbstractExternalTask
      */
     public function getConfigurableOptions()
     {
-        // TODO: Implement getConfigurableOptions() method.
-    }
+        $resolver = new OptionsResolver();
+        $resolver->setDefaults([
+            'check_patterns'  => ['*'],
+            'exclude'         => '.idea',
+            'auto_fix'        => false,
+            'dotfiles'        => false,
+            'ignore_defaults' => false,
+            'list_files'      => false,
+        ]);
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfiguration()
-    {
-        // TODO: Implement getConfiguration() method.
+        $resolver->addAllowedTypes('check_patterns', ['string[]']);
+        $resolver->addAllowedTypes('exclude', ['string']);
+        $resolver->addAllowedTypes('auto_fix', ['bool']);
+        $resolver->addAllowedTypes('dotfiles', ['bool']);
+        $resolver->addAllowedTypes('ignore_defaults', ['bool']);
+        $resolver->addAllowedTypes('list_files', ['array']);
+
+        return $resolver;
     }
 
     /**
@@ -48,14 +57,70 @@ class EditorConfig extends AbstractExternalTask
      */
     public function canRunInContext(ContextInterface $context)
     {
-        // TODO: Implement canRunInContext() method.
+        return ($context instanceof GitPreCommitContext || $context instanceof RunContext);
     }
 
     /**
      * {@inheritdoc}
+     *
+     * @SuppressWarnings(PHPMD.StaticAccess)
      */
     public function run(ContextInterface $context)
     {
-        // TODO: Implement run() method.
+        $config = $this->getConfiguration();
+        /** @var array $check_patterns */
+        $check_patterns = $config['check_patterns'];
+
+        /** @var \GrumPHP\Collection\FilesCollection $files */
+        $files = $context->getFiles();
+        if (0 !== count($check_patterns)) {
+            $files = $files->paths($check_patterns);
+        }
+        $files = $files->extensions(["*"]);
+
+        if (0 === count($files)) {
+            return TaskResult::createSkipped($this, $context);
+        }
+
+        $arguments = $this->processBuilder->createArgumentsForCommand($this->getName());
+        $arguments = $this->addArgumentsFromConfig($arguments, $config);
+        $arguments->addFiles($files);
+
+        $process = $this->processBuilder->buildProcess($arguments);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $output = $process->getErrorOutput();
+            return TaskResult::createFailed($this, $context, $output);
+        }
+
+        return TaskResult::createPassed($this, $context);
+    }
+
+    /**
+     * @param ProcessArgumentsCollection $arguments
+     *
+     * @param array                      $config
+     *
+     * @return ProcessArgumentsCollection
+     */
+    protected function addArgumentsFromConfig(
+        ProcessArgumentsCollection $arguments,
+        array $config
+    ) {
+        $arguments->addOptionalArgument('--auto-fix', $config['auto_fix']);
+
+        $arguments->addOptionalArgument('--dotfiles', $config['dotfiles']);
+        $arguments->addOptionalArgument(
+            '--ignore-defaults',
+            $config['ignore_defaults']
+        );
+        $arguments->addOptionalArgument('--list-files', $config['list_files']);
+        $arguments->addArgumentArrayWithSeparatedValue(
+            '--exclude',
+            $config['exclude']
+        );
+
+        return $arguments;
     }
 }
